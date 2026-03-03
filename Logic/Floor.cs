969510257a6122d2893aa2320326549bd6f524ea
Cody Howell <codyhowell44@gmail.com>
2026-03-02 17:52:45 -0700

    floor can be destructed

--- Logic/Floor.cs
+++ Logic/Floor.cs
@@ -6,7 +6,7 @@ namespace Logic;
 public class Floor {
     private int maxWidth;
     private Point2D[] points;
-    public IEnumerable<Point2D> GetPoints() {
+    public Point2D[] GetPoints() {
         return points;
     }
 
@@ -36,7 +36,7 @@ public class Floor {
     public bool IsUnder(Point2D point) {
         for (int i = 1; i < points.Length; i++) {
             if (point.X <= points[i].X) {
-                Equation2D e = new Equation2D(points[i-1], points[i]);
+                Equation2D e = new Equation2D(points[i - 1], points[i]);
                 double result = e.Slope * point.X + e.Intercept;
                 if (point.Y > result) return false;
                 return true;
@@ -46,12 +46,25 @@ public class Floor {
         return false;
     }
 
-    // /// <summary>
-    // /// Given a point and radius, decreases the heights of nearby points to simulate destructive environments.
-    // /// </summary>
-    // /// <param name="point">Point of the explosion</param>
-    // /// <param name="radius">Radius of the floor to remove</param>
-    // public void CalculateExplosion(Point2D point, int radius) {
-
-    // }
+    /// <summary>
+    /// Given a point and radius, decreases the heights of nearby points to simulate destructive environments.
+    /// </summary>
+    /// <param name="point">Point of the explosion</param>
+    /// <param name="radius">Radius of the floor to remove</param>
+    public void CalculateExplosion(Point2D point, int radius) {
+        IEnumerable<(Point2D point, int index)> affectedPoints = points.Select((a, i) => (a, i)).Where(a => Math.Abs(a.a.X - point.X) <= radius);
+        foreach ((Point2D p, int i) in affectedPoints) {
+            double xDistance = Math.Abs(point.X - p.X);
+            double yDistance = Math.Sqrt(Math.Pow(radius, 2) - Math.Pow(xDistance, 2));
+            if (p.Y <= point.Y) {
+                if (p.GetDistance(point) > radius) continue;
+                
+                yDistance = Math.Min(yDistance, radius - Math.Abs(p.Y - point.Y));
+                points[i] = new Point2D(p.X, p.Y - yDistance);
+            } else {
+                double removalHeight = yDistance + Math.Min(p.Y - point.Y, yDistance);
+                points[i] = new Point2D(p.X, p.Y - removalHeight);
+            }
+        }
+    }
 }
--- LogicTests/FloorTests.cs
+++ LogicTests/FloorTests.cs
@@ -7,7 +7,7 @@ public class FloorTests {
     [Test]
     public async Task FloorCoordinates() {
         Floor f = new Floor([70, 65, 80, 70, 60, 50], 100);
-        IEnumerable<Point2D> points = f.GetPoints();
+        Point2D[] points = f.GetPoints();
         await Assert.That(points).IsEquivalentTo([
             new Point2D(0, 70),
             new Point2D(20, 65),
@@ -30,5 +30,51 @@ public class FloorTests {
     public async Task PointIsUnder(double x, double y, bool exp) {
         Floor f = new Floor([70, 65, 80, 70, 60, 50], 100);
         await Assert.That(f.IsUnder(new Point2D(x, y))).IsEqualTo(exp);
+    }
+
+    [Test]
+    public async Task FloorCanBeDestructed1() {
+        Floor f = new Floor([70, 65, 80, 70, 60, 50], 100);
+        f.CalculateExplosion(new Point2D(40, 80), 10);
+        await Assert.That(f.GetPoints()[2].Y).IsEqualTo(70);
+    }
+
+    [Test]
+    public async Task FloorCanBeDestructed2() {
+        Floor f = new Floor([70, 65, 80, 70, 60, 50], 100);
+        f.CalculateExplosion(new Point2D(40, 70), 10);
+        await Assert.That(f.GetPoints()[2].Y).IsEqualTo(60);
+    }
+
+    [Test]
+    public async Task FloorCanBeDestructed3() {
+        Floor f = new Floor([70, 65, 80, 70, 60, 50], 100);
+        f.CalculateExplosion(new Point2D(40, 75), 10);
+        await Assert.That(f.GetPoints()[2].Y).IsEqualTo(65);
+    }
+
+    [Test]
+    public async Task FloorCanBeDestructed4() {
+        Floor f = new Floor([70, 65, 80, 70, 60, 50], 100);
+        f.CalculateExplosion(new Point2D(40, 85), 10);
+        await Assert.That(f.GetPoints()[2].Y).IsEqualTo(75);
+    }
+
+    [Test]
+    public async Task FloorCanBeDestructed5() {
+        Floor f = new Floor([70, 65, 80, 70, 60, 50], 100);
+        f.CalculateExplosion(new Point2D(40, 90), 10);
+        await Assert.That(f.GetPoints()[2].Y).IsEqualTo(80);
+    }
+
+    [Test]
+    public async Task FloorCanBeDestructed6() {
+        Floor f = new Floor([70, 65, 80, 70, 60, 50], 10);
+        f.CalculateExplosion(new Point2D(5, 70), 5);
+        await Assert.That(f.GetPoints()[1].Y).IsEqualTo(65);
+        await Assert.That(f.GetPoints()[2].Y).IsLessThan(80);
+        await Assert.That(f.GetPoints()[3].Y).IsLessThan(70);
+        await Assert.That(f.GetPoints()[4].Y).IsEqualTo(60);
+        await Assert.That(f.GetPoints()[5].Y).IsEqualTo(50);
     }
 }
