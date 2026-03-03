ed4a1f635e99134efbf103decbc9f9846d35e103
Cody Howell <codyhowell44@gmail.com>
2026-03-02 17:03:16 -0700

    More floor features

--- Logic/Floor.cs
+++ Logic/Floor.cs
@@ -1,24 +1,57 @@
-﻿// using HowlDev.Simulation.Physics.Primitive2D;
+﻿using System.Diagnostics;
+using HowlDev.Simulation.Physics.Primitive2D;
 
 namespace Logic;
 
 public class Floor {
     private int maxWidth;
-    private IEnumerable<int> pointHeights;
-    // public IEnumerable<Point2D> Points {
-    //     get {
-    //         return [];
-    //     }
-    // }
+    private Point2D[] points;
+    public IEnumerable<Point2D> GetPoints() {
+        return points;
+    }
 
-    // private static const int minHeight = 20;
+    private static int MinHeight => 20;
 
     /// <summary>
     /// Initializes the internal array with an array of points spaced evenly
     /// between 0 and the max size.
     /// </summary>
-    public Floor(IEnumerable<int> pointHeights, int maxWidth) {
-        this.pointHeights = pointHeights;
+    public Floor(int[] pointHeights, int maxWidth) {
+        int length = pointHeights.Length;
+        int[] x_coords = [.. MathHelpers.GetSpacedOutValues(length, maxWidth)];
+        Debug.Assert(x_coords.Length == length);
+        List<Point2D> calcPoints = [];
+        for (int i = 0; i < length; i++) {
+            calcPoints.Add(new Point2D(x_coords[i], Math.Max(pointHeights[i], MinHeight)));
+        }
+
+        points = [.. calcPoints];
         this.maxWidth = maxWidth;
     }
+
+    /// <summary>
+    /// Returns true if the provided point is beneath the floor. Defaults to False if x-coordinate 
+    /// is outside of the range.
+    /// </summary>
+    public bool IsUnder(Point2D point) {
+        for (int i = 1; i < points.Length; i++) {
+            if (point.X <= points[i].X) {
+                Equation2D e = new Equation2D(points[i-1], points[i]);
+                double result = e.Slope * point.X + e.Intercept;
+                if (point.Y > result) return false;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // /// <summary>
+    // /// Given a point and radius, decreases the heights of nearby points to simulate destructive environments.
+    // /// </summary>
+    // /// <param name="point">Point of the explosion</param>
+    // /// <param name="radius">Radius of the floor to remove</param>
+    // public void CalculateExplosion(Point2D point, int radius) {
+
+    // }
 }
--- LogicTests/FloorTests.cs
+++ LogicTests/FloorTests.cs
@@ -1,10 +1,34 @@
-﻿namespace LogicTests;
+﻿using HowlDev.Simulation.Physics.Primitive2D;
+using Logic;
 
-public class FloorTests
-{
+namespace LogicTests;
+
+public class FloorTests {
     [Test]
-    public void Basic()
-    {
-        Console.WriteLine("This is a basic test");
+    public async Task FloorCoordinates() {
+        Floor f = new Floor([70, 65, 80, 70, 60, 50], 100);
+        IEnumerable<Point2D> points = f.GetPoints();
+        await Assert.That(points).IsEquivalentTo([
+            new Point2D(0, 70),
+            new Point2D(20, 65),
+            new Point2D(40, 80),
+            new Point2D(60, 70),
+            new Point2D(80, 60),
+            new Point2D(100, 50),
+            ]);
+    }
+
+    [Test]
+    [Arguments(20, 20, true)]
+    [Arguments(20, 70, false)]
+    [Arguments(80, 70, false)]
+    [Arguments(80, 59, true)]
+    [Arguments(50, 76, false)]
+    [Arguments(50, 74, true)]
+    [Arguments(30, 70, true)]
+    [Arguments(30, 75, false)]
+    public async Task PointIsUnder(double x, double y, bool exp) {
+        Floor f = new Floor([70, 65, 80, 70, 60, 50], 100);
+        await Assert.That(f.IsUnder(new Point2D(x, y))).IsEqualTo(exp);
     }
 }
